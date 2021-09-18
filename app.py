c995249c5b5557b96d1a8c11c02eb5e635b4702f
Tarang Lunawat <tangluna@users.noreply.github.com>
2021-09-18 16:18:33 -0400

    Pulling Video to gestureTracker.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
+from gestureTracker import coordinates
 
 app = Flask(__name__)
+print('Coordinates: ', coordinates)
 
 
 @app.route('/')
-def main():
-    return render_template('index.html')
-
+def main(coordinates=coordinates):
+    return render_template('index.html', coordinates=coordinates)
 
 if __name__ == ' __main__':
     app.debug = True
