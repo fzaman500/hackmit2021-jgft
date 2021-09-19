b545e1fb618151427e4b183c74536564515ac706
Tarang Lunawat <tangluna@users.noreply.github.com>
2021-09-19 01:51:54 -0400

    1 data file CSV

--- datatSetGenerator.py
+++ datatSetGenerator.py
@@ -3,6 +3,7 @@ import cv2
 from PIL import Image as im
 import pandas as pd
 import mediapipe as mp
+import os
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
@@ -28,36 +29,52 @@ for image in images_numpy:
     break
 '''
 
+IMAGE_FILE_CONTAINER = []
+for i in range(10):
+    IMAGE_FILE_CONTAINER.append([])
+    for filename in os.listdir("./" + str(i)):
+        IMAGE_FILE_CONTAINER[i].append(filename)
 
-IMAGE_FILES = ["IMG_1140.jfif"]
+import csv
+
+# open the file in the write mode
+f = open('./data.csv', 'w')
+
 with mp_hands.Hands(
     static_image_mode=True,
     max_num_hands=2,
     min_detection_confidence=0.5) as hands:
-  for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
-    image = cv2.flip(cv2.imread(file), 1)
-    # Convert the BGR image to RGB before processing.
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+  for i in range(len(IMAGE_FILE_CONTAINER)):
+      for file in ['IMG_1140.jfif']:
+        print('file:' + file)
+        # Read an image, flip it around y-axis for correct handedness output (see
+        # above).
+        image = cv2.flip(cv2.imread(file), 1)
+        # Convert the BGR image to RGB before processing.
+        results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-    # Print handedness and draw hand landmarks on the image.
-    print('Handedness:', results.multi_handedness)
-    if not results.multi_hand_landmarks:
-      continue
-    image_height, image_width, _ = image.shape
-    annotated_image = image.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-      print('hand_landmarks:', hand_landmarks)
-      print(
-          f'Index finger tip coordinates: (',
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-      )
-      mp_drawing.draw_landmarks(
-          annotated_image,
-          hand_landmarks,
-          mp_hands.HAND_CONNECTIONS,
-          mp_drawing_styles.get_default_hand_landmarks_style(),
-          mp_drawing_styles.get_default_hand_connections_style())
-    im.fromarray(annotated_image.astype(np.uint8)).save('test2.png')
+        # Print handedness and draw hand landmarks on the image.
+        #print('Handedness:', results.multi_handedness)
+        if not results.multi_hand_landmarks:
+          continue
+        image_height, image_width, _ = image.shape
+        annotated_image = image.copy()
+        for hand_landmarks in results.multi_hand_landmarks:
+          f.write(str(i)+',\"'+str(hand_landmarks)+"\"\n")
+          #land = 'hand_landmarks:' + '   ' + hand_landmarks + '     '
+          #print('hand_landmarks:', hand_landmarks)
+          #print(
+          #    f'Index finger tip coordinates: (',
+          #    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
+          #    f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
+          #)
+          mp_drawing.draw_landmarks(
+              annotated_image,
+              hand_landmarks,
+              mp_hands.HAND_CONNECTIONS,
+              mp_drawing_styles.get_default_hand_landmarks_style(),
+              mp_drawing_styles.get_default_hand_connections_style())
+        #im.fromarray(annotated_image.astype(np.uint8)).save('test2.png')
+
+# close the file
+f.close()
